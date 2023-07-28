f9a545e09099aa1388e2dcae08e893a86016660f
olakoder <oyebanjiolatunji2014@gmail.com>
2023-07-28 09:34:42 -0700

    first-commit

---
+++ index.js
@@ -0,0 +1,57 @@
+let intro = document.querySelector ('#intro')
+
+intro.addEventListener ('click', introduction);
+
+function introduction() {
+    document.querySelector('#intro-attribute').classList.toggle('active')
+}
+
+let text = document.querySelector ('#text')
+
+text.addEventListener ('click', textFormat);
+
+function textFormat() {
+    document.querySelector('#text-formatting').classList.toggle('active')
+}
+
+let image = document.querySelector ('#images')
+
+image.addEventListener ('click', imageAttribute);
+
+function imageAttribute() {
+    document.querySelector('#image-attribute').classList.toggle('active')
+}
+
+let anchorMe = document.querySelector ('#anchor')
+
+anchorMe.addEventListener ('click', anchorAttribute);
+
+function anchorAttribute() {
+    document.querySelector('#anchor-attr').classList.toggle('active')
+}
+
+let listMe = document.querySelector ('#list')
+list.addEventListener ('click', listAttribute);
+function listAttribute() {
+    document.querySelector('#list-attribute').classList.toggle('active')
+}
+
+let frameMe = document.querySelector ('#frame')
+frameMe.addEventListener ('click', frameAttributes);
+function frameAttributes() {
+    document.querySelector('#frame-attributes').classList.toggle('active')
+}
+
+let tableMe = document.querySelector ('#table')
+tableMe.addEventListener ('click', tableAttributes);
+function tableAttributes() {
+    document.querySelector('#table-attributes').classList.toggle('active')
+}
+
+let formMe = document.querySelector ('#form')
+formMe.addEventListener ('click', formAttributes);
+function formAttributes() {
+    document.querySelector('#form-attributes').classList.toggle('active')
+}
+
+
